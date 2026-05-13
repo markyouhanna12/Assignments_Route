935f5c1d19b73598f271ab8c37c3cdc270761e9c
markyouhanna12 <markyouhanna1@gmail.com>
2026-05-13 19:17:40 +0300

    Bonus part

---
+++ Assignment_8/bonus.js
@@ -0,0 +1,27 @@
+var romanToInt = function(s) {
+    const roman = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+
+    let total = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        let current = roman[s[i]];
+        let next = roman[s[i + 1]];
+
+        // If current value is smaller than next value, subtract it
+        if (current < next) {
+            total -= current;
+        } else {
+            total += current;
+        }
+    }
+
+    return total
+}
