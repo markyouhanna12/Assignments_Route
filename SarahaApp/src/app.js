2f7d940326e419824eac726348385b2c16648d6a
markyouhanna12 <markyouhanna1@gmail.com>
2026-07-08 16:24:13 +0300

    feat: implement logging middleware for auth and user routes

--- SarahaApp/src/app.js
+++ SarahaApp/src/app.js
@@ -6,15 +6,18 @@ import userRouter from "./modules/User/user.controller.js"
 import cors from "cors"
 import helmet from "helmet"
 import { corsOptions } from "./utils/cors/cors.utils.js"
+import { attachRouterWithLogger } from "./logger/morgan.logger.js"
+
 const app = express()
 
 app.use(express.json())
 app.use(cors(corsOptions()))
 app.use(helmet())
 
+attachRouterWithLogger(app , "/auth" ,authRouter , "auth.log" )
+attachRouterWithLogger(app , "/user" ,userRouter , "user.log" )
+
 await connectDB()
-app.use("/auth",authRouter)
-app.use("/user",userRouter)
 
 app.all("/*dummy",(req,res)=>{
     throw NotFoundException({messaage:"Not found Handler!!"})
