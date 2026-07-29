9cdb0131842d5f5cbfd3971bd1b67f899d0d8da7
markyouhanna12 <markyouhanna1@gmail.com>
2026-07-29 23:40:08 +0300

    feat: implement database connection and authentication module structure

--- Social Media/dist/app.controller.js
+++ Social Media/dist/app.controller.js
@@ -9,11 +9,15 @@ const helmet_1 = __importDefault(require("helmet"));
 const cors_utils_1 = require("./Utils/cors/cors.utils");
 const rateLimitter_middleware_1 = require("./Middlewares/rateLimitter.middleware");
 const error_response_1 = require("./Utils/response/error.response");
+const connection_1 = __importDefault(require("./DB/connection"));
+const auth_controller_1 = __importDefault(require("./Modules/Auth/auth.controller"));
 const app = (0, express_1.default)();
+(0, connection_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)(cors_utils_1.corsOptions));
 app.use((0, helmet_1.default)());
 app.use(rateLimitter_middleware_1.customRateLimiter);
+app.use('/api/auth', auth_controller_1.default);
 app.use(error_response_1.globalErrorHandler);
 app.use('/*dummy', (req, res) => {
     throw new error_response_1.NotFoundException('Not Found Handler!');
--- Social Media/src/app.controller.ts
+++ Social Media/src/app.controller.ts
@@ -4,13 +4,19 @@ import helmet from 'helmet';
 import { corsOptions } from './Utils/cors/cors.utils';
 import { customRateLimiter } from './Middlewares/rateLimitter.middleware';
 import { globalErrorHandler, NotFoundException } from './Utils/response/error.response';
+import connectDB from './DB/connection';
+import AuthRouter from './Modules/Auth/auth.controller';
 
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(helmet());
 app.use(customRateLimiter);
+
+app.use('/api/auth', AuthRouter);
 
 app.use(globalErrorHandler);
 
