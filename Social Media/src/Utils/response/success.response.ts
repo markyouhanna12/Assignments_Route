8e03f49ee3e5c7037417085e3fb56443bccfbf7d
markyouhanna12 <markyouhanna1@gmail.com>
2026-07-29 22:05:39 +0300

    feat: refactor success response utility to improve type safety and structure

--- Social Media/src/Utils/response/success.response.ts
+++ Social Media/src/Utils/response/success.response.ts
@@ -1,1 +1,17 @@
-import { Request, Response, NextFunction } from 'express';
+import { Response } from 'express';
+
+interface SuccessResponseParams {
+  res: Response;
+  statusCode?: number;
+  message?: string;
+  data?: unknown;
+}
+
+export const successResponse = ({
+  res,
+  statusCode = 200,
+  message = 'Done',
+  data = {},
+}: SuccessResponseParams): Response => {
+  return res.status(statusCode).json({ message, data });
+};
