fd54e57e6b32f29c6c803f3bdb6826e15e2a4cc4
markyouhanna12 <markyouhanna1@gmail.com>
2026-07-31 20:20:19 +0300

    feat: add authentication and authorization middleware with user role validation

---
+++ Social Media/src/Middlewares/Auth.middleware.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from 'express';
+import { RoleEnum, TokenTypeEnum } from '../Utils/enums/auth.enum';
+import { TokenService } from '../Utils/services/token';
+import { BadRequestException, ForbiddenException } from '../Utils/response/error.response';
+
+export const authentication = ({ tokenType = TokenTypeEnum.ACCESS }) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const tokenService = new TokenService();
+
+    if (!req.headers.authorization) {
+      throw new BadRequestException('authorization header is missing');
+    }
+    const { user, decoded } =
+      (await tokenService.decodedToken({
+        authorization: req.headers.authorization,
+        tokenType,
+      })) || {};
+
+    req.user = user;
+    req.decoded = decoded;
+  };
+};
+
+export const authorization = ({ accessRoles = [] }: { accessRoles?: RoleEnum[] }) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user.role || !accessRoles.includes(req.user.role)) {
+      throw new ForbiddenException('you are not authorized to access this route');
+    }
+
+    return next();
+  };
+};
