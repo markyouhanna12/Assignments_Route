881ede3e0ab6666eeaea360f54e1108bb3137247
markyouhanna12 <markyouhanna1@gmail.com>
2026-08-07 00:49:59 +0300

    feat: integrate Firebase for notification services
    - Added Firebase and Firebase Admin SDK to package.json.
    - Created notification.config.ts to initialize Firebase Admin with service account credentials.
    - Implemented NotificationService class in notification.service.ts for sending single and bulk notifications.

---
+++ Social Media/src/Utils/services/notification.config.ts
@@ -0,0 +1,19 @@
+import admin from 'firebase-admin';
+import { readFileSync } from 'node:fs';
+import { resolve } from 'node:path';
+
+const serviceAccount = JSON.parse(
+  readFileSync(
+    resolve('config\social-media-app-79e10-firebase-adminsdk-fbsvc-2a87e16a91.json'),
+    'utf-8',
+  ),
+);
+
+const firebaseApp =
+  admin.apps.length > 0
+    ? admin.app()
+    : admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+      });
+
+export default firebaseApp;
