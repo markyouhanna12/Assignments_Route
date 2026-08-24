278524a9cd773379053ed5f03511de40cdcce939
markyouhanna12 <markyouhanna1@gmail.com>
2026-08-25 00:56:02 +0300

    feat: integrate socket.io for real-time connection handling and logging

--- Social Media/dist/server.js
+++ Social Media/dist/server.js
@@ -7,11 +7,23 @@ const chalk_1 = __importDefault(require("chalk"));
 const http_1 = require("http");
 const config_service_1 = require("./config/config.service");
 const app_controller_1 = __importDefault(require("./app.controller"));
+const socket_io_1 = require("socket.io");
 const httpServer = (0, http_1.createServer)(app_controller_1.default);
 const startServer = async () => {
     try {
         httpServer.listen(config_service_1.PORT, () => {
             console.log(chalk_1.default.bold.blue(`HTTP server running on port ${config_service_1.PORT}`));
+        });
+        const io = new socket_io_1.Server(httpServer, {
+            cors: {
+                origin: '*',
+            },
+        });
+        io.on('connection', (socket) => {
+            console.log(socket.id);
+            socket.on('disconnect', () => {
+                console.log(`Logout from ${socket.id}`);
+            });
         });
     }
     catch (error) {
--- Social Media/src/server.ts
+++ Social Media/src/server.ts
@@ -2,6 +2,7 @@ import chalk from 'chalk';
 import { createServer } from 'http';
 import { PORT } from './config/config.service';
 import app from './app.controller';
+import { Server, Socket } from 'socket.io';
 
 const httpServer = createServer(app);
 
@@ -9,6 +10,20 @@ const startServer = async () => {
   try {
     httpServer.listen(PORT, () => {
       console.log(chalk.bold.blue(`HTTP server running on port ${PORT}`));
+    });
+
+    const io = new Server(httpServer, {
+      cors: {
+        origin: '*',
+      },
+    });
+
+    io.on('connection', (socket: Socket) => {
+      console.log(socket.id);
+
+      socket.on('disconnect', () => {
+        console.log(`Logout from ${socket.id}`);
+      });
     });
   } catch (error) {
     console.log(chalk.red(String(error)));
