4358730562e443dfe1f3f5cb9fa62ec8fdbb03e2
markyouhanna12 <markyouhanna1@gmail.com>
2026-09-01 03:27:33 +0300

    feat: implement message model with schema and validation for MongoDB

---
+++ Social Media/src/DB/Models/message.model.ts
@@ -0,0 +1,67 @@
+import { HydratedDocument, Model, model, Schema, Types } from 'mongoose';
+
+export interface IMessage {
+  _id: Types.ObjectId;
+  conversationId: Types.ObjectId;
+
+  senderId: Types.ObjectId;
+  recieverId: Types.ObjectId;
+
+  content: string;
+  readAt: Date;
+
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export const messageSchema = new Schema<IMessage>(
+  {
+    content: {
+      type: String,
+      minLength: 1,
+      maxLength: 50000,
+      required: true,
+    },
+    senderId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    recieverId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    conversationId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Conversation',
+      required: true,
+    },
+    readAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+messageSchema.index({
+  conversationId: 1,
+  createdAt: -1,
+});
+
+messageSchema.index({
+  recieverId: 1,
+  readAt: -1,
+});
+
+messageSchema.pre('validate', async function () {
+  if (this.content) {
+    this.content = this.content.trim();
+  }
+});
+
+export const MessageModel: Model<IMessage> = model<IMessage>('Message', messageSchema);
+
+export type HMessageDocument = HydratedDocument<IMessage>;
--- Social Media/src/server.ts
+++ Social Media/src/server.ts
@@ -11,20 +11,6 @@ const startServer = async () => {
     httpServer.listen(PORT, () => {
       console.log(chalk.bold.blue(`HTTP server running on port ${PORT}`));
     });
-
-    const io = new Server(httpServer, {
-      cors: {
-        origin: '*',
-      },
-    });
-
-    io.on('connection', (socket: Socket) => {
-      console.log(socket.id);
-
-      socket.on('disconnect', () => {
-        console.log(`Logout from ${socket.id}`);
-      });
-    });
   } catch (error) {
     console.log(chalk.red(String(error)));
   }
