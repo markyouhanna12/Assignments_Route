6c1e7edacdbbee52dc3e4eb1ff4676b4c7a61920
markyouhanna12 <markyouhanna1@gmail.com>
2026-09-03 07:15:02 +0300

    feat: add mongoose integration and database connection setup

---
+++ Wazfnee/src/DB/connection.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import chalk from 'chalk';
+import { dbUrl } from '../Config/config.service';
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on('connected', () => {
+      console.log(chalk.bold.green('MongoDB connected successfully'));
+    });
+    mongoose.connect(dbUrl, {
+      serverSelectionTimeoutMS: 5000,
+    });
+  } catch (error) {
+    console.log(chalk.red('Error connecting Database'), error);
+  }
+};
+
+export default connectDB;
--- Wazfnee/src/app.controller.ts
+++ Wazfnee/src/app.controller.ts
@@ -4,8 +4,11 @@ import helmet from 'helmet';
 import { corsOptions } from './Utils/cors/cors.utils';
 import { NotFoundException } from './Utils/response/error.response';
 import { customRateLimiter } from './Middlewares/rateLimitter.middleware';
+import connectDB from './DB/connection';
 
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 app.use(cors(corsOptions));
