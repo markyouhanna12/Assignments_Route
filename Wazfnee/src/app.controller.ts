22f2f308fea674af86df8169f42752565373268a
markyouhanna12 <markyouhanna1@gmail.com>
2026-09-03 06:56:12 +0300

    feat: implement custom rate limiter middleware for IP request handling

--- Wazfnee/src/app.controller.ts
+++ Wazfnee/src/app.controller.ts
@@ -3,12 +3,14 @@ import cors from 'cors';
 import helmet from 'helmet';
 import { corsOptions } from './Utils/cors/cors.utils';
 import { NotFoundException } from './Utils/response/error.response';
+import { customRateLimiter } from './Middlewares/rateLimitter.middleware';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(helmet());
+app.use(customRateLimiter);
 
 app.use('/*dummy', (req: Request, res: Response): Response => {
   throw new NotFoundException('Not Found Handler!');
