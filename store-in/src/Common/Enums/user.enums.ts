7099f661672bd1df7c8fde5e6159a924fca6fa7e
markyouhanna12 <markyouhanna1@gmail.com>
2026-09-25 20:00:44 +0300

    feat: add user-related enums for role, gender, provider, and authorization types

---
+++ store-in/src/Common/Enums/user.enums.ts
@@ -0,0 +1,19 @@
+export enum RoleEnum {
+  ADMIN = 'ADMIN',
+  USER = 'USER',
+}
+
+export enum GenderEnum {
+  MALE = 'MALE',
+  FEMALE = 'FEMALE',
+}
+
+export enum ProviderEnum {
+  GOOGLE = 'GOOGLE',
+  SYSTEM = 'SYSTEM',
+}
+
+export enum AuthorizationType {
+  USER = 'USER',
+  ADMIN = 'ADMIN',
+}
