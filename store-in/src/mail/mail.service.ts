375e9273334068f05efecfd7c7dc61eabf99958b
markyouhanna12 <markyouhanna1@gmail.com>
2026-09-25 21:41:34 +0300

    feat(mail): add email verification service with OTP functionality
    - Added MailModule to handle email sending using @nestjs-modules/mailer.
    - Implemented MailService to send verification OTP emails.
    - Created EJS template for OTP email.
    - Updated package.json to include necessary dependencies for mailer.

---
+++ store-in/src/mail/mail.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
+
+@Injectable()
+export class MailService {
+  private readonly logger = new Logger(MailService.name);
+  constructor(private readonly mailerService: MailerService) {}
+
+  async sendVerificationOtp(email: string, otp: string): Promise<void> {
+    try {
+      await this.mailerService.sendMail({
+        to: email,
+        subject: 'Your activation Code',
+        template: './otp.ejs',
+        context: { confirmEmailOTP: otp },
+      });
+      this.logger.log(`Verfication code successfully sent to : ${email}`);
+    } catch (error) {
+      this.logger.error(`Failed to send email to : ${email}`);
+    }
+  }
+}
