f8f79b8a4fc48cfbfd3181c73fb8f82491a11fcb
markyouhanna12 <markyouhanna1@gmail.com>
2026-09-26 05:38:35 +0300

    feat: update Nest CLI configuration and enhance main application bootstrap

--- store-in/src/main.ts
+++ store-in/src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 500);
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+  });
+
+  await app.listen(process.env.PORT ?? 5000, () => {
+    console.log(`Server is running on port ${process.env.PORT ?? 5000}`);
+  });
 }
 bootstrap();
